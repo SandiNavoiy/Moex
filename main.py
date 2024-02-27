9df8b28be64aeb323e607b1ab9173f250038a40d
Alex <santes.au@gmail.com>
2024-02-27 19:12:28 +0300

    Вывод графика с произвольными датами

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stock.parser import plot_stock
 
 if __name__ == '__main__':
-    plot_stock()
+    name_stock = {1: "YNDX", 2: "ROSN"}
+    plot_stock(name_stock[2])
--- stock/parser.py
+++ stock/parser.py
@@ -1,11 +1,22 @@
+import datetime
+
 import pandas as pd
 import requests
 from matplotlib import pyplot as plt
 
-def plot_stock():
+def plot_stock(name):
     """Вывод графика функции"""
+    date1 = datetime.date(2023, 11, 27)
+    date2 = datetime.date(2023, 12, 30)
+
     # Запрос
-    j = requests.get('http://iss.moex.com/iss/engines/stock/markets/shares/securities/YNDX/candles.json?from=2023-05-25&till=2023-09-01&interval=24').json()
+    respose = requests.get(f'http://iss.moex.com/iss/engines/stock/markets/shares/securities/{name}/candles.json?from={date1}&till={date2}&interval=24')
+    #interval – Размер свечки - целое число 1 (1 минута), 10 (10 минут),
+    # 60 (1 час), 24 (1 день), 7 (1 неделя), 31 (1 месяц) или 4 (1 квартал). По умолчанию дневные данные
+    if respose.status_code == 200:
+        # Преобразование JSON в словарь
+        j = respose.json()
+
     # Загоняем в пандас
     data = [{k : r[i] for i, k in enumerate(j['candles']['columns'])} for r in j['candles']['data']]
     frame = pd.DataFrame(data)
@@ -13,7 +24,8 @@ def plot_stock():
     plt.plot(list(frame['close']))
     #Сохранение акции
     plt.savefig("shares.png")
-    plt.title("Стоимость акции")
-    plt.xlabel("Дни")
+    plt.title(f"Стоимость акции {name}, рублей")
+    plt.xlabel(f"C {date1} до {date2}")
     plt.ylabel("Стоимость")
+    plt.grid(color='pink')
     plt.show()
