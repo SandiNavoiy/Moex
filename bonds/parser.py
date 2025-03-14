2497e89eb61b7112a89e7179862454b8e31d4497
Alex <santes.au@gmail.com>
2025-03-14 13:48:17 +0300

    Начнем все заново

--- bonds/parser.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-def get_bonds():
-    """Парсим бонды"""
-    start_time = datetime.datetime.now()
-    for p in range(1, 1000):
-        bonds = moex.get_bonds(p, 100)
--- main.py
+++
@@ -1,7 +0,0 @@
-from stock.parser import plot_stock, coast_stock
-
-if __name__ == '__main__':
-    name_stock = {1: "YNDX", 2: "ROSN"}
-    plot_stock(name_stock[2])
-
-    coast_stock(name_stock[2])
