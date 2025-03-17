255f0ccf6f078ffeaa54f79d88ef6a9eedcad993
Alex <santes.au@gmail.com>
2025-03-17 12:19:21 +0300

    Начнем все заново+

--- stock/parser.py
+++
@@ -1,9 +0,0 @@
-from pprint import pprint
-
-import requests
-
-url = "https://iss.moex.com/iss/index.json?iss.only=markets"
-response = requests.get(url)  # запрос данных по url
-
-
-pprint(response.json())
