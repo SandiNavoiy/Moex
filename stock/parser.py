4f6bc06379f0be4c12871a1f52f23dced3014712
Sandi_Navoiy <santes.au@gmail.com>
2025-03-23 21:15:55 +0300

    Получение данных м биржи, улучшения

--- stock/parser.py
+++ stock/parser.py
@@ -2,6 +2,11 @@ import time
 import requests
 import pandas as pd
 
+
+# Настройки pandas для отображения всех столбцов
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 200)
+
 def get_moex_securities(market):
     """Функия получения данных от API Мсбиржи"""
     # Получаем данные по указанному рынку (расширенный формат)
@@ -9,7 +14,7 @@ def get_moex_securities(market):
     # Получаем данные построчно, пока не закончатся данные по рынку
     # Если результат пустой, возвращаем None
 
-    # ��ормат URL для получения данных от API Мосбиржи:
+    # Формат URL для получения данных от API Мосбиржи:
     # https://iss.moex.com/iss/securities.json?engine=stock&market={market}
     # engine - указывает вид торговых операций
     url = f"https://iss.moex.com/iss/securities.json?engine=stock&market={market}"
@@ -18,20 +23,20 @@ def get_moex_securities(market):
 
     while True:
         try:
-            response = requests.get(f"{url}&start={start}", timeout=10)
-            response.raise_for_status()  # Вызывает исключение при ошибке HTTP
+            response = requests.get(f"{url}&start={start}", timeout=30)
+            # timeout=30 выбран опытным путем
             data = response.json()
         except requests.exceptions.RequestException as e:
-            print(f"Ошибка подключения: {e}")
+            print(f"Ошибка подключения:  - выключи vpn {e}")
             time.sleep(5)  # Пауза перед повтором
             continue
 
-        columns = data.get('securities', {}).get('columns', [])
-        rows = data.get('securities', {}).get('data', [])
-
+        columns = data.get('securities').get('columns')
+        rows = data.get('securities').get('data')
+        # выход из цикла
         if not rows:
             break
-
+        # добавляем полученные данные в общий список
         all_data.extend(rows)
         start += 100
 
